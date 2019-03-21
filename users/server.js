77ae749ded61bcc94e3af15850710e0c163152aa
coreycosman <cosman.corey@gmail.com>
2019-03-21 21:32:10 +0400

    init

---
+++ users/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+const expressGraphQL = require("express-graphql");
+const schema = require("./schema/schema");
+
+app.use("/graphql", expressGraphQL({ schema, graphiql: true }));
+
+app.listen(5000, () => {
+  console.log("listening on 5000");
+});
